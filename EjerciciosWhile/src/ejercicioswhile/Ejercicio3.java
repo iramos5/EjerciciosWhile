2fd205158c06a3ad7d25e47f243a9df7ce93c8af
iramos <iramos@106-24.info2>
2022-11-02 13:41:33 +0100

    .

--- EjerciciosWhile/src/ejercicioswhile/Ejercicio3.java
+++ EjerciciosWhile/src/ejercicioswhile/Ejercicio3.java
@@ -3,7 +3,10 @@ package ejercicioswhile;
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* Escribe un programa que vaya pidiendo al usuario números enteros positivos que debe ir sumando. 
+		 * Cuando el usuario no quiera insertar más números, introducirá un número negativo y el algoritmo, 
+		 * antes de acabar, mostrará la media de los números positivos introducidos por el usuario.
+		 */
 
 	}
 
