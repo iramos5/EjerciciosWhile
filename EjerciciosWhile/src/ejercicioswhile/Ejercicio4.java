a5df93d89a989ade851ba7c7ae4c1fa7ddbef7e1
iramos <iramos@106-24.info2>
2022-11-07 08:50:28 +0100

    mas adelante

---
+++ EjerciciosWhile/src/ejercicioswhile/Ejercicio4.java
@@ -0,0 +1,12 @@
+package ejercicioswhile;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		/* Escribe un programa que vaya pidiendo al usuario números y muestre por pantalla si el número es par o impar 
+		 * (indistintamente que sea positivo o negativo). El algoritmo terminará cuando el usuario introduzca el número cero.
+		 */
+
+	}
+
+}
