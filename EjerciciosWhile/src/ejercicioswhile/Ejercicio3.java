57183e0559a0e35511d9b8a43054554bc4e949ed
iramos <iramos@106-24.info2>
2022-11-07 08:48:02 +0100

    sin  terminar

--- EjerciciosWhile/src/ejercicioswhile/Ejercicio3.java
+++ EjerciciosWhile/src/ejercicioswhile/Ejercicio3.java
@@ -1,4 +1,5 @@
 package ejercicioswhile;
+import java.util.Scanner;
 
 public class Ejercicio3 {
 
@@ -7,6 +8,26 @@ public class Ejercicio3 {
 		 * Cuando el usuario no quiera insertar más números, introducirá un número negativo y el algoritmo, 
 		 * antes de acabar, mostrará la media de los números positivos introducidos por el usuario.
 		 */
+		
+		// variables
+		Scanner scan = new Scanner(System.in); //iniciar escáner
+		int num, media; //numeros
+		
+		//introducir datos
+		System.out.println("Introduce un número positivo: "); //pedir numero positivo
+		num = scan.nextInt(); //guardar numero positivo
+		
+		//while
+		while (num>0) {
+			System.out.println("Introduce otro número positivo y cuando quieras finalizar el programa introduce un numero negativo");
+			num = scan.nextInt();
+		}
+		
+		media = (num+num)/num;
+		
+		System.out.println("Has salido del programa");
+		System.out.println("Media de números positivos introducidos: " + media);
+		
 
 	}
 
