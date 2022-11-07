5ad474b4c8224ad2ea6f660409b4b63061766864
iramos <iramos@106-24.info2>
2022-11-07 09:25:45 +0100

    solo enunciado

---
+++ EjerciciosWhile/src/ejercicioswhile/Ejercicio7.java
@@ -0,0 +1,14 @@
+package ejercicioswhile;
+
+public class Ejercicio7 {
+
+	public static void main(String[] args) {
+		/* Codificar el juego “el número secreto”, que consiste en acertar un número entre 1 y 100 (generado aleatoriamente). 
+		 * Para ello se introduce por teclado una serie de números, para los que se indica: “mayor” o “menor”, según sea mayor o menor 
+		 * con respecto al número secreto. El proceso termina cuando el usuario acierta o cuando se rinde (introduciendo un -1).
+		 */
+
+
+	}
+
+}
